a9d945dff34c5e6d0cb61c305869e06edf221793
Filip Urban <filipurbanzadania@wp.pl>
2026-03-30 21:42:44 +0200

    feat: hello world

---
+++ target/debug/deps/rastatui-420d7aea86c0606e.d
@@ -0,0 +1,5 @@
+/Users/filipurban/Desktop/Projects/restatui/target/debug/deps/rastatui-420d7aea86c0606e: src/main.rs
+
+/Users/filipurban/Desktop/Projects/restatui/target/debug/deps/rastatui-420d7aea86c0606e.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rastatui.d
@@ -0,0 +1,1 @@
+/Users/filipurban/Desktop/Projects/restatui/target/debug/rastatui: /Users/filipurban/Desktop/Projects/restatui/src/main.rs
